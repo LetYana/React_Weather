6e68f76360bfb3ccb8abe5915aaabb3008ea9c16
LetYana <114302100+LetYana@users.noreply.github.com>
2023-01-28 20:09:23 +0100

    creating a skeleton html page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,103 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="paragraph">Hello!</p>
-      </header>
+      <div className="citySearch">
+        <img className="searchIcon" href="#" alt="City Search" />
+        <input className="cityInput" type="search" placeholder="Enter a city" />
+        <input className="citySubmit" type="submit" />
+      </div>
+
+      <div className="dateData">
+        <p className="dateYear">January 28, 2023</p>
+        <h1 className="cityName">Amsterdam</h1>
+        <p className="dayWeekTime">Saturday 19:21</p>
+      </div>
+
+      <div className="dayWeather">
+        <div>
+          <img className="mainWeatherIcon" href="#" alt="Weather" />
+        </div>
+        <div>
+          <h2>12°C</h2>
+          <h3>Broken clouds</h3>
+        </div>
+      </div>
+
+      <div className="dayWeatherDetails">
+        <div className="detailsWind">
+          <img className="detailsIcon" href="#" alt="Weather" />
+          <p className="detailsProperty">Wind</p>
+          <p className="detailsValue">11 km/h</p>
+        </div>
+        <div className="detailsHumidity">
+          <img className="detailsIcon" href="#" alt="Weather" />
+          <p className="detailsProperty">Humidity</p>
+          <p className="detailsValue">77%</p>
+        </div>
+        <div className="detailsUVIndex">
+          <img className="detailsIcon" href="#" alt="Weather" />
+          <p className="detailsProperty">UV Index</p>
+          <p className="detailsValue">0 of 10</p>
+        </div>
+        <div className="detailsPressure">
+          <img className="detailsIcon" href="#" alt="Weather" />
+          <p className="detailsProperty">Pressure</p>
+          <p className="detailsValue">1030.1 mb</p>
+        </div>
+      </div>
+
+      <div className="weatherForecast">
+        <p>Forecast for next 6 days</p>
+        <div>
+          <div>
+            <p className="forecastDay">Mon</p>
+            <img className="forecastIcon" href="#" alt="Weather" />
+            <p className="forecastTempMax">12°C</p>
+            <p className="forecastTempMin">9°C</p>
+          </div>
+          <div>
+            <p className="forecastDay">Mon</p>
+            <img className="forecastIcon" href="#" alt="Weather" />
+            <p className="forecastTempMax">12°C</p>
+            <p className="forecastTempMin">9°C</p>
+          </div>
+          <div>
+            <p className="forecastDay">Mon</p>
+            <img className="forecastIcon" href="#" alt="Weather" />
+            <p className="forecastTempMax">12°C</p>
+            <p className="forecastTempMin">9°C</p>
+          </div>
+          <div>
+            <p className="forecastDay">Mon</p>
+            <img className="forecastIcon" href="#" alt="Weather" />
+            <p className="forecastTempMax">12°C</p>
+            <p className="forecastTempMin">9°C</p>
+          </div>
+          <div>
+            <p className="forecastDay">Mon</p>
+            <img className="forecastIcon" href="#" alt="Weather" />
+            <p className="forecastTempMax">12°C</p>
+            <p className="forecastTempMin">9°C</p>
+          </div>
+          <div>
+            <p className="forecastDay">Mon</p>
+            <img className="forecastIcon" href="#" alt="Weather" />
+            <p className="forecastTempMax">12°C</p>
+            <p className="forecastTempMin">9°C</p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <p>
+          <a href="https://github.com/LetYana/React_Weather">
+            Open-source code on GitHub
+          </a>
+          by letYana
+        </p>
+      </div>
     </div>
   );
 }
