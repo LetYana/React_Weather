02c1c112cf05480f1fd3efd5a61070958f70dcea
LetYana <114302100+LetYana@users.noreply.github.com>
2023-02-09 19:09:29 +0100

    codeMapping for icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,45 +2,40 @@ import React from "react";
 import FormattedDate from "./FormattedDate.js";
 import FormattedFirstdate from "./FormattedFirstdate.js";
 import WeatherTemperature from "./WeatherTemperature";
+import WeatherIcon from "./WeatherIcon.js";
 
 import wind from "./wind.svg";
 import vmc from "./vmc.svg";
 import humidity from "./humidity.svg";
 import pressure from "./pressure.svg";
-// import icon_50d from "./icon_50d.svg";
-// import icon_13d from "./icon_13d.svg";
-// import icon_11d from "./icon_11d.svg";
-import icon_10d from "./icon_10d.svg";
-// import icon_09d from "./icon_09d.svg";
-// import icon_04d from "./icon_04d.svg";
-// import icon_03d from "./icon_03d.svg";
-// import icon_02d from "./icon_02d.svg";
-import icon_01d from "./icon_01d.svg";
 
 export default function WeatherInfo(props) {
   return (
     <div>
       <div className="dateData">
-        <p className="dateYear">
+        <span className="dateYear">
           <FormattedFirstdate date={props.data.date} />
-        </p>
+        </span>
         <h1 className="cityName">{props.data.city}</h1>
-        <p className="dayWeekTime">
+        <span className="dayWeekTime">
           <FormattedDate date={props.data.date} />
-        </p>
+        </span>
       </div>
 
       <div className="dayWeather">
         <div className="dayWeatherImage">
-          <img
+          <WeatherIcon
+            code={props.data.icon}
+            description={props.data.description}
+          />
+          {/* <img
             className="dayWeatherIcon"
             src={props.data.icon}
             alt={props.data.description}
-          />
+          /> */}
         </div>
         <div className="dayWeatherTemp">
           <WeatherTemperature celsius={props.data.temperature} />
-          {/* <h2>{Math.round(props.data.temperature)}°C</h2> */}
           <h3>{props.data.description}</h3>
         </div>
       </div>
@@ -73,31 +68,31 @@ export default function WeatherInfo(props) {
         <div className="weatherForecastDiv">
           <div>
             <p className="forecastDay">Mon</p>
-            <img className="forecastIcon" src={props.data.icon} alt="Weather" />
+            <img className="forecastIcon" src={wind} alt="Weather" />
             <p className="forecastTempMax">12°</p>
             <p className="forecastTempMin">9°</p>
           </div>
           <div>
             <p className="forecastDay">Mon</p>
-            <img className="forecastIcon" src={props.data.icon} alt="Weather" />
+            <img className="forecastIcon" src={wind} alt="Weather" />
             <p className="forecastTempMax">12°</p>
             <p className="forecastTempMin">9°</p>
           </div>
           <div>
             <p className="forecastDay">Mon</p>
-            <img className="forecastIcon" src={icon_01d} alt="Weather" />
+            <img className="forecastIcon" src={wind} alt="Weather" />
             <p className="forecastTempMax">12°</p>
             <p className="forecastTempMin">9°</p>
           </div>
           <div>
             <p className="forecastDay">Mon</p>
-            <img className="forecastIcon" src={icon_01d} alt="Weather" />
+            <img className="forecastIcon" src={wind} alt="Weather" />
             <p className="forecastTempMax">12°</p>
             <p className="forecastTempMin">9°</p>
           </div>
           <div>
             <p className="forecastDay">Mon</p>
-            <img className="forecastIcon" src={icon_10d} alt="Weather" />
+            <img className="forecastIcon" src={wind} alt="Weather" />
             <p className="forecastTempMax">12°</p>
             <p className="forecastTempMin">9°</p>
           </div>
